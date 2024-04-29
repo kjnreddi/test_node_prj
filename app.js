7bb12ba5605e702a049a4478a4adde9a52e167e2
kjnreddi <kjnreddi@gmail.com>
2024-04-29 11:25:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Worlde!');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
